6b867e10308563acbabe61ebed078fc9282539bb
Denis Zakharov <d.zakharov@bssys.com>
2020-07-16 11:30:22 +0300

    Initial server

---
+++ src/Application.kt
@@ -0,0 +1,58 @@
+package com.exxbrain
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.content.TextContent
+import io.ktor.features.*
+import io.ktor.gson.GsonConverter
+import io.ktor.gson.gson
+import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.withCharset
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+enum class ServerStatus {OK}
+data class Health(val status: ServerStatus)
+
+@kotlin.jvm.JvmOverloads
+fun Application.main(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) {
+            call.respond(TextContent("${it.value} ${it.description}",
+                ContentType.Text.Plain.withCharset(Charsets.UTF_8), it))
+        }
+    }
+    install(CallLogging)
+    install(ConditionalHeaders)
+    install(AutoHeadResponse)
+    install(CORS) {
+        anyHost()
+        allowCredentials = true
+        listOf(HttpMethod("PATCH"), HttpMethod.Put, HttpMethod.Delete).forEach {
+            method(it)
+        }
+    }
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, GsonConverter())
+        gson {
+            setPrettyPrinting()
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+        get("/health") {
+            call.respond(Health(status = ServerStatus.OK))
+        }
+    }
+}
+
